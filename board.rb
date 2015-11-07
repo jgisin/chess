323a0f917e9f13c10bf661b743fc23a0bb7d671e
Jeffrey Gisin <jgisin@gmail.com>
2015-11-07 00:13:37 -0700

    Board

---
+++ board.rb
@@ -0,0 +1,28 @@
+class Board
+	  def initialize
+    @attributes = {a: [(1..8).to_a],
+			 	   b: [(1..8).to_a],
+			 	   c: [(1..8).to_a],
+			 	   d: [(1..8).to_a],
+			 	   e: [(1..8).to_a],
+			 	   f: [(1..8).to_a],
+			 	   g: [(1..8).to_a],
+			 	   h: [(1..8).to_a]}
+    @attributes.each do |k,v|
+      self.class.send :define_method, k do v end
+    end
+  end
+  def display_board
+  	@attributes.values.each do |val|
+     val[0].each do
+  		print "_ "
+  	end
+  	print "\n"
+  end
+  end
+end
+
+
+board = Board.new
+
+board.display_board
